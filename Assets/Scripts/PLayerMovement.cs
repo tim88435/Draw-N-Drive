17a6f6ba49152795c609f30333a0758d04cc66fb
tim88435 <timofey.kond@gmail.com>
2022-08-30 15:01:25 +1000

    Update PLayerMovement.cs

--- Assets/Scripts/PLayerMovement.cs
+++ Assets/Scripts/PLayerMovement.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PLayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Singleton
+    private PlayerMovement _singleton;
+    public PlayerMovement Singleton
     {
-        
+        get { return _singleton; }
+        set
+        {
+            if (_singleton == null)
+            {
+                _singleton = value;
+            }
+            else
+            {
+                Debug.LogWarning($"{nameof(value)} already exists in the current scene. Deleting clone");
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    private void OnValidate()
+    {
+        Singleton = this;
+    }
+    #endregion
+    private float _speed;
+    public float Speed
+    {
+        get { return _speed; }
+        set
+        {
+            _speed = value;
+        }
+    }
+    private int _health;
+    public int Health
+    {
+        get { return _health; }
+        set
+        {
+            _health = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
