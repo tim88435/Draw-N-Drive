2147afe11b570bf98f66bb6e3e8373e2bd01e29f
tim88435 <timofey.kond@gmail.com>
2022-10-30 00:48:53 +1100

    Finalized Bike Animation Code
    Also added the card textures
    Animations now use two curves, depending on how fast the player is moving

--- Assets/Scripts/BikeAnimations.cs
+++ Assets/Scripts/BikeAnimations.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BikeAnimations : MonoBehaviour
@@ -25,23 +23,41 @@ public class BikeAnimations : MonoBehaviour
     #endregion
     #region Variables
     [SerializeField] private Transform bikeHandles, bikeFrontWheel, bikeBackWheel;
-    [Range(0, 90)] [SerializeField] private float bikeEulerAngleVertical;
-    [Range(0, 90)] [SerializeField] private float bikeEulerAngleHorizontal;
-    [Range(0, 90)] [SerializeField] private float handleEulerAngle;
-    private float speedRatio
-    {
-        get
-        {
-            return PlayerMovement.Singleton.Velocity.z * 0.02f;
-        }
-    }
+    [Tooltip("The angle that the bike will turn in the direction that it is driving")]
+    [SerializeField] private AnimationCurve turnAngle;
+    [Tooltip("The angle that the bike will 'angle' itself to make turning easier")]
+    [SerializeField] private AnimationCurve bikeAngle;
+    [Tooltip("The angle that the front wheel and the handles will turn with the player's input")]
+    [SerializeField] private AnimationCurve handlesAngle;
     #endregion
     private void FixedUpdate()
     {
-        transform.localRotation = Quaternion.Euler(0, 180 + PlayerMovement.Singleton.Velocity.normalized.x * bikeEulerAngleHorizontal, Input.GetAxis("Horizontal") * bikeEulerAngleVertical * speedRatio * speedRatio);
-        bikeHandles.localRotation = Quaternion.Euler(0, PlayerMovement.Singleton.Velocity.normalized.x * handleEulerAngle, 0);
+        //y is rotating bike in the direction of movement
+        //z is rotating the bike to be on an angle          Angle \/                                                                          ratio depending on current speed \/                      Angle \/                                                                          ratio depending on current speed \/
+        transform.localRotation = Quaternion.Euler(0, 180 + ReverseAngle(Quaternion.Euler(PlayerMovement.Singleton.Velocity).eulerAngles.x) * turnAngle.Evaluate(PlayerMovement.Singleton.Velocity.z), ReverseAngle(Quaternion.Euler(PlayerMovement.Singleton.Velocity).eulerAngles.x) * bikeAngle.Evaluate(PlayerMovement.Singleton.Velocity.z));
+        //front wheel and handle rotation               Angle \/                                                                          ratio depending on current speed \/
+        bikeHandles.localRotation = Quaternion.Euler(0, ReverseAngle(Quaternion.Euler(PlayerMovement.Singleton.Velocity).eulerAngles.x) * handlesAngle.Evaluate(PlayerMovement.Singleton.Velocity.z), 0);
+        //wheel spin
         bikeFrontWheel.Rotate(Vector3.right, -PlayerMovement.Singleton.Velocity.z * Time.deltaTime * 90 * GameManager.Singleton.GameSpeed, Space.Self);
         bikeBackWheel.Rotate(Vector3.right, -PlayerMovement.Singleton.Velocity.z * Time.deltaTime * 90 * GameManager.Singleton.GameSpeed, Space.Self);
         //Mathf.Pow(50 - speedRatio, (float)System.Math.E) * 0.001f + 15;
+    }
+    /// <summary>
+    /// Changes the angle to be from a 360 degree angle to a 180 to -180 angle
+    /// </summary>
+    /// <param name="original"></param>
+    /// <returns>180 to -180 angle</returns>
+    private float ReverseAngle(float original)
+    {
+        original = original % 360;//makes sure that it's between 360 and -360
+        if (original >= 180)//check for over 180
+        {
+            original -= 360;
+        }
+        else if (original <= -180)//check for under -180
+        {
+            original += 360;
+        }
+        return original;
     }
 }
